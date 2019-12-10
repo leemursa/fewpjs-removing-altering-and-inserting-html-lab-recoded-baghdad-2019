2c028ba83833958efe3304c5f59e5fa24364da07
Mursaleen Taqi <leemursa@gmail.com>
2019-12-10 07:31:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+document.getElementById("main").remove();
+
+let element = document.ctreatElementr("h1");
+document.body.appendChild(element);
+document.grtElementByTagName("h1").setAttribute("id","victory");
+let newHeader = document.getElementById("victory");
+let NAME ="seen"
+document.getElementById("victory").innerHTML=`${NAME} is the champi0n`;
